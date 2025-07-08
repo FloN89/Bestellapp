421587f1db31dcfb9bdacca20531d9d714b18e5e
Florian Narr <f.narr@gmx.de>
2025-07-08 18:32:43 +0200

    dishes added/css first style added

---
+++ script.js
@@ -0,0 +1,19 @@
+let mainDishes = [
+    {"name": "Schnitzel",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Leberkas",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Schweinsbraten",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Kasspatzen",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+];
+
