1ed302417cce1dc8cb6c2fd26e2ee6aff53a0434
Florian Narr <f.narr@gmx.de>
2025-07-22 20:23:03 +0200

    changed structur

---
+++ scripts/food.js
@@ -0,0 +1,68 @@
+let dishes = [
+    {"name": "Schnitzel",
+     "price": 16.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+    {"name": "Leberkas",
+     "price": 15.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+    {"name": "Schweinsbraten",
+     "price": 19.99, 
+     "description": "schnitzel ..............." 
+    
+    },
+    {"name": "Kasspatzen",
+     "price": 12.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+];
+ let desserts = [
+    {"name": "Apfelkuchen",
+     "price": 5.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+    {"name": "Eis-Palatschinken",
+     "price": 6.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+    {"name": "Schoko-Mousse",
+     "price": 5.99, 
+     "description": "schnitzel ..............." 
+    
+    },
+    {"name": "Obstteller",
+     "price": 4.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+];
+let drinks = [
+    {"name": "Cola",
+     "price": 2.99, 
+     "description": "schnitzel ..............." 
+    
+    },
+    {"name": "Almdudler",
+     "price": 2.99, 
+     "description": "schnitzel ..............." 
+    
+    },
+    {"name": "Helles",
+     "price": 3.99, 
+     "description": "schnitzel ..............." 
+    
+    },
+    {"name": "Tafelwasser",
+     "price": 1.99, 
+     "description": "schnitzel ..............." 
+     
+    },
+];
+
+
