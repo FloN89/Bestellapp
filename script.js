2be6e0a7ae1106f3933d690d5d3659cdbd5d3e29
Florian Narr <f.narr@gmx.de>
2025-07-22 19:18:48 +0200

    added new images

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-let mainDishes = [
+let dishes = {
+"Hauptgerichte": [
+    {"name": "Schnitzel",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Leberkas",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Schweinsbraten",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Kasspatzen",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+];
+"Desserts": [
+    {"name": "Schnitzel",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Leberkas",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Schweinsbraten",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+    {"name": "Kasspatzen",
+     "price": 12.99, 
+     "description": "schnitzel ..............."  
+    },
+];
+"Getränke": [
     {"name": "Schnitzel",
      "price": 12.99, 
      "description": "schnitzel ..............."  
@@ -17,3 +54,4 @@ let mainDishes = [
     },
 ];
 
+}
